78a94d87a496fe7d6bebc8e083ed326ecb9fe2ed
Carly Donais <carlydonais@Carlys-MBP.lan>
2022-09-14 10:16:37 -0400

    added contact, portfolio, resume index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
 import './App.css';
 
 function App() {
@@ -9,6 +12,9 @@ function App() {
       <Nav></Nav>
       <main>
       <About></About>
+      <Contact></Contact>
+      <Portfolio></Portfolio>
+      <Resume></Resume>
       </main>
     </div>
   );
