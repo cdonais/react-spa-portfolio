8188a02802e0b986215496b4b1bc1503deb8a7b0
Carly Donais <carlydonais@Carlys-MBP.lan>
2022-09-14 10:12:08 -0400

    about index

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Nav from './components/Nav';
+import About from './components/About';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
+      <main>
+      <About></About>
+      </main>
     </div>
   );
 }
