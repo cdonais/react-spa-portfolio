c32bb9379f30530e5ae48277d7774ec3acac6062
Carly Donais <carlydonais@Carlys-MBP.lan>
2022-09-14 09:57:23 -0400

    nav

---
+++ src/components/Nav/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+
+function Nav() {
+    return (
+        <header className='flex-row px-1'>
+            <h2>
+                <a href="/">
+                     Carly Donais
+                </a>
+            </h2>
+            <nav>
+                <ul className='flex-row'>
+                    <li className='mx-2'>
+                        <a href="#about">
+                            About me
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#contact">
+                            Contact
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#portfolio">
+                            Portfolio
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#resume">
+                            Resume
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Nav;
