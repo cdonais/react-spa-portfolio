ceae0a1f03fca5087bebf54eb0fa539039d280d6
Carly Donais <carly.donais@gmail.com>
2022-09-17 16:28:15 -0400

    added photo

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import IMG_7745 from "../../assets/IMG_7745.jpeg"
 
 
 function About() {
     return (
         <section>
         <h2>About Me</h2>
-        <p>My name is Carly Donais, I am a full stack developer living in Orlando, Florida. In my spare time I enjoy watching sports, reading, and spending time with my two children.</p>
+        <img src={IMG_7745} className="my-2" style={{ width: "25%" }} alt="cover" />
+        <p>My name is Carly Donais, I am a full stack web developer and sports content writer living in Orlando, Florida. In my spare time I enjoy watching sports, reading, and spending time with my two children.</p>
         </section>
     )
 }
