bd3e7a436db30b9a1ecbbe19c1402dbc87bdc090
Carly Donais <carly.donais@gmail.com>
2022-09-17 23:04:08 -0400

    added portfolio photos

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import CatDog from "../../assets/catanddog.jpeg";
+import RunBuddy from "../../assets/run-buddy.jpeg";
+import Florida from "../../assets/florida.jpeg";
 
 function Portfolio() {
     return (
         <section>
         <h2>Portfolio</h2>
+        <img src={RunBuddy}className="my-2" style={{ width: "25%" }} alt="cover" />
+        <img src={CatDog}className="my-2" style={{ width: "25%" }} alt="cover" />
+        <img src={Florida}className="my-2" style={{ width: "25%" }} alt="cover" />
         </section>
     )
 }
